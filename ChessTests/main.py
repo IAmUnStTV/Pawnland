4dfaf258ebd7c52ed57b4e303eb6518bed299419
IAmUnStTV <unsttv@icloud.com>
2021-02-06 11:07:45 +0000

    Made selector

--- ChessTests/main.py
+++ ChessTests/main.py
@@ -2,20 +2,34 @@ import chess
 import chess.svg
 import webbrowser as web
 
-rook = chess.svg.piece(chess.Piece.from_symbol("R"))
+ans = str(input('What chess piece do you want (Rook, Pawn, King, Queen, Knight or Bishop)? '))
+
+if ans == 'Rook':
+    cp = chess.svg.piece(chess.Piece.from_symbol("R"))
+elif ans == 'Pawn':
+    cp = chess.svg.piece(chess.Piece.from_symbol("P"))
+elif ans == 'King':
+    cp = chess.svg.piece(chess.Piece.from_symbol("K"))
+elif ans == 'Queen':
+    cp = chess.svg.piece(chess.Piece.from_symbol("Q"))
+elif ans == 'Knight':
+    cp = chess.svg.piece(chess.Piece.from_symbol("N"))
+elif ans == 'Bishop':
+    cp = chess.svg.piece(chess.Piece.from_symbol("B"))
+else:
+    cp = 'Error: Couldn\'t read, please run code again'
+
 f = open("svgtest.html", "w")
 html = '''
 <html>
-    <head>
-        <title>SVG Test: Rook</title>
-    </head>
-    <body>
-        <h1>SVG Test In HTML: Rook</h1>
-        <p>A rook should appear below</p>''' + rook +'''
-    </body>
+<head>
+<title>SVG Test:'''+ ans +'''</title>
+</head>
+<body>
+<h1>SVG Test In HTML:'''+ ans +'''
+<p>A '''+ ans +''' should appear below</p>'''+ cp +'''
+</body>
 </html>'''
 f.write(html)
 f.close()
-
-#open and read the file after the appending:
 web.open('svgtest.html')
